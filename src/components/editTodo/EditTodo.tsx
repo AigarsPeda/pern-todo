65f9de839eac1ad21cbd61036db6040a0e096ad4
Aigars Peda <aigarspeda@gmail.com>
2020-10-29 09:34:48 +0200

    Update EditTodo.tsx

--- src/components/editTodo/EditTodo.tsx
+++ src/components/editTodo/EditTodo.tsx
@@ -1,21 +1,63 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { BASE_URL } from "../../constants";
+import { TodoType } from "../../types";
 
-const EditTodo: React.FC = () => {
+type Props = {
+  todo: TodoType;
+};
+
+const EditTodo: React.FC<Props> = (props) => {
+  const { todo } = props;
   const [isOpen, setIsOpen] = useState(false);
+  const [toggle, setToggle] = useState(false);
+  const [description, setDescription] = useState("");
+
+  useEffect(() => {
+    setDescription(todo.description);
+  }, [todo.description]);
 
   const handleOpen = () => {
     setIsOpen(true);
+    setToggle(true);
   };
 
   const handleClose = () => {
-    setIsOpen(false);
+    setToggle(false);
+
+    setTimeout(() => {
+      setIsOpen(false);
+    }, 500);
   };
 
   const handleOutClick = (e: React.MouseEvent<HTMLDivElement>) => {
     const element = e.target as HTMLDivElement;
 
-    if (element.classList.contains("edit-modal")) {
-      setIsOpen(false);
+    if (element.classList.contains("edit-modal-background")) {
+      setToggle(false);
+
+      setTimeout(() => {
+        setIsOpen(false);
+      }, 500);
+    }
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const body = { description };
+    try {
+      fetch(`${BASE_URL}/todos/${todo.todo_id}`, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body)
+      });
+
+      setToggle(false);
+      setTimeout(() => {
+        setIsOpen(false);
+        window.location.href = "/";
+      }, 500);
+    } catch (error) {
+      console.error(error.message);
     }
   };
 
@@ -24,11 +66,27 @@ const EditTodo: React.FC = () => {
       <button className="edit-button" onClick={handleOpen}>
         Edit
       </button>
+
       {isOpen && (
-        <div className="edit-modal" onClick={handleOutClick}>
-          <div className="edit-form">
-            <button onClick={handleClose}>Close</button>
+        <div className="edit-modal-background" onClick={handleOutClick}>
+          <div
+            className={toggle ? "edit-modal toggleIn" : "edit-modal toggleOut"}
+          >
+            <div className="close-button-container">
+              <button onClick={handleClose} className="close-button">
+                X
+              </button>
+            </div>
+
             <h1>Edit Todo</h1>
+            <form onSubmit={handleSubmit}>
+              <input
+                type="text"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+              />
+              <button type="submit">Done</button>
+            </form>
           </div>
         </div>
       )}
